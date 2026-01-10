3cc9cf8abc7406bd9a51040122eca5a10eee2855
Feliex127 <laiyouxian5@gmail.com>
2026-01-10 11:24:34 +0800

    Add files via upload

--- src/main/java/com/feliex/serverbalancer/manager/WorldSettings.java
+++ src/main/java/com/feliex/serverbalancer/manager/WorldSettings.java
@@ -1,3 +1,22 @@
+package com.feliex.serverbalancer.manager;
+
 public class WorldSettings {
-    
+    private boolean pvpEnabled = false;
+    private boolean sharedInventory = false;
+
+    public boolean isPvpEnabled() {
+        return pvpEnabled;
+    }
+
+    public void setPvpEnabled(boolean pvpEnabled) {
+        this.pvpEnabled = pvpEnabled;
+    }
+
+    public boolean isSharedInventory() {
+        return sharedInventory;
+    }
+
+    public void setSharedInventory(boolean sharedInventory) {
+        this.sharedInventory = sharedInventory;
+    }
 }
